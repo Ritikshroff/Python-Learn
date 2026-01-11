1e4f996b296b85e643efada4077e5520a3653529
Ritikkumar88 <chomu4321ritik@gmail.com>
2026-01-11 14:14:38 +0530

    array np

--- Array.py
+++ Array.py
@@ -1,1 +1,97 @@
 # Hello world
+import array
+
+val = array.array('i', [1, 2, 3, 4, 5])
+
+# print(val)
+# print(val.typecode, "type of array")
+# print(val.buffer_info(), "buffer info")
+# print(val.reverse(), "reverse")
+
+for i in val:
+    print(i , end=" | ") 
+
+print("\n")
+val.reverse()
+for i in val:
+    print(i , end=" / ") 
+
+print("\n")
+
+val.insert(2, 90)
+for i in val:
+    print(i , end=" | ") 
+    
+print("\n")
+
+
+val.append(100)
+val[4] = 10
+for i in val:
+    print(i , end=" | ") 
+
+print("\n")
+
+copyArray = array.array(val.typecode, (x*3 for x in val))
+for i in copyArray:
+    print(i , end=" | ")
+
+
+print("\n")
+copyArray.insert(0 , 101)
+for i in copyArray:
+    print(i , end=" | ")
+    
+print("\n")
+
+for i in val:
+    print(i , end=" | ")
+
+print("\n")
+
+
+copyArray.pop(3)
+for i in copyArray:
+    print(i , end=" | ")
+    
+print("\n")
+
+
+slicing  = copyArray[2:5]
+for i in slicing:
+    print(i , end=" | ")
+
+print("\n")
+print("\n")
+print("\n")
+print("\n")
+
+
+
+newArr = array.array('i' , [23, 32, 1, 2, 3, 4, 5, 5])
+for i in newArr:
+    print(i , end=" | ")
+
+
+print("\n")
+
+abc = newArr[::-1]
+for i in abc:
+    print(i , end=" | ")
+
+
+
+# inputArr = array.array('i', [])
+# n = int(input("Enter the length of array : "))
+# for i in range(n):
+#     x = int(input("Enter the value : "))
+#     inputArr.append(x)
+
+# for i in inputArr:
+#     print( i , end=" | ")
+
+
+print("\n")
+
+printindex = newArr.index(5)
+print(printindex)
---
+++ numpyArray.py
@@ -0,0 +1,32 @@
+# # import numpy as np
+# from numpy import *
+
+
+# val = array([1, 2, 3, 4, 5])
+# for x in range(0, len(val)):
+#     print(x , end=" | ")
+
+
+import numpy as np
+
+# val = np.array([1, 2, 3, 4, 5, 0.9 , 4.4, 44, 'a', True])
+# print(val)
+
+# newval = np.linspace(10,23, 5)
+# print(newval)
+
+
+# arrangearray = np.arange(10, 20 , 2)
+# print(arrangearray)
+
+
+
+# multidimensional array
+zerodimentional = np.array(10)
+oneD = np.array([1,2,3,4,5])
+twoD = np.array([[1,2,3,4,5] , [6,7,8,9,10]])
+threeD = np.array([[[1,2,3,4,5] , [6,7,8,9,10]]])
+print(zerodimentional)
+print(oneD)
+print(twoD)
+print(threeD)
